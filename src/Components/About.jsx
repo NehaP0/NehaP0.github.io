83523777f29c4ed08290e5f543ce915cb8a738a5
NehaP0 <nehaphadtare334@gmail.com>
2023-03-22 20:22:07 +0530

    commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ joining a progressive organization and continuing to
 grow my skillset while contributing to a positive
 outcome.</p>
 
-<button><Link to='https://drive.google.com/file/d/1DmwDd8Pjxf10tOUOrrivF9y5xZ7Egtir/view?usp=share_link' id="resume-button-2"  target={'_blank'}>Resume</Link></button>
+<button id="resume-button-2" ><Link to='https://drive.google.com/file/d/1DmwDd8Pjxf10tOUOrrivF9y5xZ7Egtir/view?usp=share_link'  target={'_blank'}>Resume</Link></button>
 
     {/* download as pdf is left */}
 
--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Resume = () => {
   return (
     <div>
-      <button><Link to='https://drive.google.com/file/d/1DmwDd8Pjxf10tOUOrrivF9y5xZ7Egtir/view?usp=share_link' id="resume-button-1"  target={'_blank'}>Resume</Link></button>
+      <button id="resume-button-1" ><Link to='https://drive.google.com/file/d/1DmwDd8Pjxf10tOUOrrivF9y5xZ7Egtir/view?usp=share_link'  target={'_blank'}>Resume</Link></button>
         
     {/* download as pdf is left */}
     </div>
