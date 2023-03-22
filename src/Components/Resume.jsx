b1391bf6887595b350b0323d647f8abe0298ffcd
NehaP0 <nehaphadtare334@gmail.com>
2023-03-22 20:17:29 +0530

    commit

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Resume = () => {
   return (
     <div>
-        <a href="https://drive.google.com/file/d/1DmwDd8Pjxf10tOUOrrivF9y5xZ7Egtir/view?usp=share_link" id="resume-button-1" target={'_blank'}>Resume</a>
+      <button><Link to='https://drive.google.com/file/d/1DmwDd8Pjxf10tOUOrrivF9y5xZ7Egtir/view?usp=share_link' id="resume-button-1"  target={'_blank'}>Resume</Link></button>
+        
     {/* download as pdf is left */}
     </div>
   )
