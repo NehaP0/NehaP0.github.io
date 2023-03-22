43146f9d90bfea3d25875619e7a97a7fd9865e45
NehaP0 <nehaphadtare334@gmail.com>
2023-03-22 08:26:22 +0530

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,12 +4,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div id="nav-menu" style={{border:"1px solid black", display:"flex", justifyContent:"space-evenly"}} >
-     <a href="#about">About</a>
-     <a href="#skills">Skills</a>
-     <a href="#resume">Resume</a>
-     <a href="#projects">Projects</a>
+      <a href="#home"  class="nav-link home">Home</a>
+     <a href="#about" class="nav-link about">About</a>
+     <a href="#skills"  class="nav-link skills">Skills</a>
+     <a href="#resume"  class="nav-link resume">Resume</a>
+     <a href="#projects" class="nav-link projects">Projects</a>
      <a href="#github">Github</a>
-     <a href="#contacts">Contacts</a>
+     <a href="#contacts" class="nav-link contact">Contacts</a>
     </div>
   )
 }
